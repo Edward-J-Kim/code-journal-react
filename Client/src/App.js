bbaaf70b7df04e04e3f448f9c51061b23f03fedc
Edward Kim <ewakim90@gmail.com>
2023-06-30 19:31:13 +0000

    Updated src

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Header from './Components/Header';
-
+import Form from './Components/Form';
 function App() {
   return (
     <div className="header-container">
       <Header />
+      <Form />
     </div>
   );
 }
---
+++ Client/src/Components/Form.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import placehoderimage from '../images/placeholder-image-square.jpg';
+export default function Form() {
+  const [url, setUrl] = useState('');
+  return (
+    <main>
+      <form id="entryForm">
+        <div class="row margin-bottom-1">
+          <div class="column-half">
+            <img
+              class="input-b-radius form-image"
+              id="formImage"
+              src={placehoderimage}
+              alt="image of entry image"
+            />
+          </div>
+          <div class="column-half">
+            <label class="margin-bottom-1 d-block" for="title">
+              Title
+            </label>
+            <input
+              required
+              class="input-b-color text-padding input-b-radius purple-outline input-height margin-bottom-2 d-block width-100"
+              type="text"
+              id="formTitle"
+              name="formTitle"
+            />
+            <label class="margin-bottom-1 d-block" for="photoUrk">
+              Photo URL
+            </label>
+            <input
+              required
+              class="input-b-color text-padding input-b-radius purple-outline input-height margin-bottom-2 d-block width-100"
+              type="text"
+              id="formURL"
+              name="formURL"
+            />
+            onChange =
+          </div>
+        </div>
+        <div class="row margin-bottom-1">
+          <div class="column-full">
+            <label class="margin-bottom-1 d-block" for="formNotes">
+              Notes
+            </label>
+            <textarea
+              required
+              class="input-b-color text-padding input-b-radius purple-outline d-block width-100"
+              name="formNotes"
+              id="formNotes"
+              cols="30"
+              rows="10"></textarea>
+          </div>
+        </div>
+        <div class="row">
+          <div class="column-full d-flex justify-between">
+            <button
+              class="invisible delete-entry-button"
+              type="button"
+              id="deleteEntry">
+              Delete Entry
+            </button>
+            <button class="input-b-radius text-padding purple-background white-text">
+              SAVE
+            </button>
+          </div>
+        </div>
+      </form>
+    </main>
+  );
+}
